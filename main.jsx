785b4eb01b1f11e08fbb7fc49ceff0227ed704d8
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-20 11:34:08 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -8,3 +8,75 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <div
+                    style={{
+                        display: "flex",
+                        background: "black",
+                        padding: "5px 0 5px 5px",
+                        fontSize: "20px",
+                    }}
+                >
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "greenyellow"
+                                    : "white",
+                            })}
+                        >
+                            Home
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/about"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "greenyellow"
+                                    : "white",
+                            })}
+                        >
+                            About
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/contact"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "greenyellow"
+                                    : "white",
+                            })}
+                        >
+                            Contact
+                        </NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Home />}
+                    />
+                    <Route
+                        exact
+                        path="/about"
+                        element={<About />}
+                    />
+                    <Route
+                        exact
+                        path="/contact"
+                        element={<Contact />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
