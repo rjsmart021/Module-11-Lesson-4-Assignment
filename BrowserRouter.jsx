8e5f12f285aaf52858d18b66f9d6c2093ee2199b
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-20 11:20:19 -0600

    Update BrowserRouter.jsx

--- BrowserRouter.jsx
+++ BrowserRouter.jsx
@@ -1,1 +1,13 @@
-;
+// src/BrowserRouter Page.jsx
+import React from 'react';
+
+const BrowserRouter = () => {
+  return (
+    <div>
+      <h1>BrowserRouter Page</h1>
+    </div>
+  );
+};
+
+export default BrowserRouter Page;
+
