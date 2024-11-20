bb43eb9ffa8e1e9d7a7e0ece12efbe72bebefaa6
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-20 11:37:40 -0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -34,5 +34,77 @@ const App = () => {
     </Router>
   );
 };
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <div
+                    style={{
+                        display: "flex",
+                        background: "black",
+                        padding: "5px 0 5px 5px",
+                        fontSize: "20px",
+                    }}
+                >
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "greenyellow"
+                                    : "white",
+                            })}
+                        >
+                            Home
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/about"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "greenyellow"
+                                    : "white",
+                            })}
+                        >
+                            About
+                        </NavLink>
+                    </div>
+                    <div style={{ margin: "10px" }}>
+                        <NavLink
+                            to="/contact"
+                            style={({ isActive }) => ({
+                                color: isActive
+                                    ? "greenyellow"
+                                    : "white",
+                            })}
+                        >
+                            Contact
+                        </NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Home />}
+                    />
+                    <Route
+                        exact
+                        path="/BrowseCharacters"
+                        element={<BrowseCharacters />}
+                    />
+                    <Route
+                        exact
+                        path="/Comics"
+                        element={<Comics />}
+                
+                    />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
 
+export default App;
 export default App;
