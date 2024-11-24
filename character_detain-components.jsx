0e9f35c60e901a23a2935cced46d361f1c521682
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-24 00:52:16 -0600

    Create character_detain-components.jsx

---
+++ character_detain-components.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CharacterDetail from './character-detail';
+function CharacterDetails() {
+    return (
+        <div>
+            <CharacterDetail />
+        </div>
+    );
+};
+export default CharacterDetails;
