79d834f3bf33f03ddeb26480950697f81f173dff
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-11-24 00:45:59 -0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,110 +1,23 @@
-import './App.css'
-import Header from './components/header';
-import About from './components/About';
-import Contact from './components/Contact';
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './home';
+import Comics from './comics';
+import BrowseCharacters from './browse-characters';
+import NavigationBar from './navigation-bar';
+import CharacterDetailsWrapper from './character-details-wrapper';
+import NotFound from './NotFound';
+import './AppStyles.css';
 function App() {
   return (
-    <div className="App">
-    <Header ></Header>
-    <div className="main-content">
-      <About ></About>
-      <Contact ></Contact>
-    </div>
-  </div>
-  )
-}
-
-export default App
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-
-const App = () => {
-  return (
-    <Router>
+    <div>
+      <NavigationBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path='/home/' element={<Home />}/>
+        <Route path='/comics/' element={<Comics />}/>
+        <Route path='/browse-characters/' element={<BrowseCharacters />}/>
+        <Route path='/character-details/:id' element={<CharacterDetailsWrapper />}/>
+        <Route path='*' element={<NotFound />}/>
       </Routes>
-    </Router>
+    </div>
   );
 };
-function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <div
-                    style={{
-                        display: "flex",
-                        background: "black",
-                        padding: "5px 0 5px 5px",
-                        fontSize: "20px",
-                    }}
-                >
-                    <div style={{ margin: "10px" }}>
-                        <NavLink
-                            to="/"
-                            style={({ isActive }) => ({
-                                color: isActive
-                                    ? "greenyellow"
-                                    : "white",
-                            })}
-                        >
-                            Home
-                        </NavLink>
-                    </div>
-                    <div style={{ margin: "10px" }}>
-                        <NavLink
-                            to="/about"
-                            style={({ isActive }) => ({
-                                color: isActive
-                                    ? "greenyellow"
-                                    : "white",
-                            })}
-                        >
-                            About
-                        </NavLink>
-                    </div>
-                    <div style={{ margin: "10px" }}>
-                        <NavLink
-                            to="/contact"
-                            style={({ isActive }) => ({
-                                color: isActive
-                                    ? "greenyellow"
-                                    : "white",
-                            })}
-                        >
-                            Contact
-                        </NavLink>
-                    </div>
-                </div>
-                <Routes>
-                    <Route
-                        exact
-                        path="/"
-                        element={<Home />}
-                    />
-                    <Route
-                        exact
-                        path="/BrowseCharacters"
-                        element={<BrowseCharacters />}
-                    />
-                    <Route
-                        exact
-                        path="/Comics"
-                        element={<Comics />}
-                
-                    />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
-}
-
-export default App;
 export default App;
